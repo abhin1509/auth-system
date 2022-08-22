c4ac93218845e2029b07d1119f88a0cfff7c38d6
Abhinav Kumar <abhin1509@gmail.com>
2022-08-22 20:58:12 +0530

    database connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("./config/database").connect();
 
 const express = require("express");
 const app = express();
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
   //validation check using mongoose 
@@ -20,7 +21,7 @@ app.post("/register", (req, res) => {
     res.status(400).send("All fields are mandatory");
   }
 
-  const existingUser = User.findOne({ email });
+  const existingUser = await User.findOne({ email }); //PROMISE 
   if(existingUser) {
     res.status(401).send("User already exists");
   }
