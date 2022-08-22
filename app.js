b86d13c7be46e9f187cb804a5ee4553d4519b19c
Abhinav Kumar <abhin1509@gmail.com>
2022-08-22 20:20:18 +0530

    registering a user

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-require('dotenv').config();
- 
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const User = require("./model/user");
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system");
+});
+
+app.post("/register", (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  //validation check using mongoose 
+  //This is a simple validation check
+  if (!(firstname && lastname && email && password)) {
+    res.status(400).send("All fields are mandatory");
+  }
+
+  const existingUser = User.findOne({ email });
+  if(existingUser) {
+    res.status(401).send("User already exists");
+  }
+
+  //take care of password
+  //generate token or send success message
 });
 
 module.exports = app;
