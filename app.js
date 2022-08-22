7d1c6d283c8054a3687fe48b8b3b83b787f870d4
Abhinav Kumar <abhin1509@gmail.com>
2022-08-22 21:20:19 +0530

    password encryption using bcrypt

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 require("./config/database").connect();
 
 const express = require("express");
+const bcrypt = require("bcryptjs");
+
 const app = express();
 
 app.use(express.json());
@@ -15,18 +17,26 @@ app.get("/", (req, res) => {
 app.post("/register", async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
-  //validation check using mongoose 
+  //validation check using mongoose
   //This is a simple validation check
   if (!(firstname && lastname && email && password)) {
     res.status(400).send("All fields are mandatory");
   }
 
-  const existingUser = await User.findOne({ email }); //PROMISE 
-  if(existingUser) {
+  const existingUser = await User.findOne({ email }); //PROMISE
+  if (existingUser) {
     res.status(401).send("User already exists");
   }
 
   //take care of password
+  const encPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    password: encPassword,
+  });
+
   //generate token or send success message
 });
 
